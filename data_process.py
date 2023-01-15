29680e4440254a4a415bd6da93d9f89ae7a4d261
SJ X <1785541656@qq.com>
2023-01-15 12:37:53 +0800

    data2

--- data_process.py
+++ data_process.py
@@ -3,108 +3,15 @@ import networkx as nx
 import scipy.sparse as sp
 from sklearn import neighbors
 import matplotlib.pyplot as plt
-import torch
 import torch.nn as nn
 import scipy.io as sio
-import random
-import dgl
+from utils import *
 from sklearn.preprocessing import MinMaxScaler
 '''
 读取数据，处理成->(节点-子图对）
-'''
 dataset=["cora","ACM","Flickr","pubmed","citeseer","BlogCatalog"]
-
-#region
-def normalize_adj(adj):#D^(-1/2)*A*D^(-1/2)
-    """Symmetrically normalize adjacency matrix."""
-    adj = sp.coo_matrix(adj)
-    rowsum = np.array(adj.sum(1))
-    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
-
-def preprocess_adj(adj):#D^(-1/2)*(A+E)*D^(-1/2)
-    """Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
-    adj_normalized = normalize_adj(adj + sp.eye(adj.shape[0]))
-    return sparse_to_tuple(adj_normalized)
-
-def load_data(dataset):
-    data_mat = sio.loadmat(f'dataset/{dataset}.mat')
-    adj = sp.csr_matrix(data_mat['Network'])
-    feat = sp.lil_matrix(data_mat['Attributes'])
-    truth = data_mat['Label']
-    truth = truth.flatten()
-    return feat, truth, adj
-
-def sparse_to_tuple(sparse_mx, insert_batch=False):
-    """Convert sparse matrix to tuple representation."""
-    """Set insert_batch=True if you want to insert a batch dimension."""
-    def to_tuple(mx):
-        if not sp.isspmatrix_coo(mx):
-            mx = mx.tocoo()
-        if insert_batch:
-            coords = np.vstack((np.zeros(mx.row.shape[0]), mx.row, mx.col)).transpose()
-            values = mx.data
-            shape = (1,) + mx.shape
-        else:
-            coords = np.vstack((mx.row, mx.col)).transpose()
-            values = mx.data
-            shape = mx.shape
-        return coords, values, shape
-
-    if isinstance(sparse_mx, list):
-        for i in range(len(sparse_mx)):
-            sparse_mx[i] = to_tuple(sparse_mx[i])
-    else:
-        sparse_mx = to_tuple(sparse_mx)
-
-    return sparse_mx
-
-def dense_to_one_hot(labels_dense, num_classes):#离散特征的取值之间没有大小的意义
-    """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = np.arange(num_labels) * num_classes
-    labels_one_hot = np.zeros((num_labels, num_classes))
-    labels_one_hot.flat[index_offset+labels_dense.ravel()] = 1
-    return labels_one_hot
-
-def adj_to_dgl_graph(adj):
-    """Convert adjacency matrix to dgl format."""
-    nx_graph = nx.from_scipy_sparse_matrix(adj)
-    dgl_graph = dgl.DGLGraph(nx_graph)
-    return dgl_graph
-
-def generate_rwr_subgraph(dgl_graph, subgraph_size):#输出子图
-    """Generate subgraph with RWR algorithm."""
-    all_idx = list(range(dgl_graph.number_of_nodes()))
-    reduced_size = subgraph_size - 1
-    traces = dgl.contrib.sampling.random_walk_with_restart(dgl_graph, all_idx, restart_prob=1, max_nodes_per_seed=subgraph_size*3)
-    subv = []
-    for i,trace in enumerate(traces):
-        subv.append(torch.unique(torch.cat(trace),sorted=False).tolist())
-        retry_time = 0
-        while len(subv[i]) < reduced_size:
-            cur_trace = dgl.contrib.sampling.random_walk_with_restart(dgl_graph, [i], restart_prob=0.9, max_nodes_per_seed=subgraph_size*5)
-            subv[i] = torch.unique(torch.cat(cur_trace[0]),sorted=False).tolist()
-            retry_time += 1
-            if (len(subv[i]) <= 2) and (retry_time >10):
-                subv[i] = (subv[i] * reduced_size)
-        subv[i] = subv[i][:reduced_size]
-        subv[i].append(i)
-    return subv
-
-def preprocess_features(features):#特征矩阵归一化(可以换)
-    """Row-normalize feature matrix and convert to tuple representation"""
-    rowsum = np.array(features.sum(1))
-    r_inv = np.power(rowsum, -1).flatten()
-    r_inv[np.isinf(r_inv)] = 0.
-    r_mat_inv = sp.diags(r_inv)
-    features = r_mat_inv.dot(features)
-
-    return features.todense(), sparse_to_tuple(features)
-#endregion
-
+**Flickr换成minmax试试**
+'''
 class RealDataset():
     def __init__(self,dataset):
         feat, truth, adj=load_data(dataset)
@@ -112,5 +19,35 @@ class RealDataset():
         self.feat=feat
         self.truth=truth #标签
         self.norm_adj=preprocess_adj(adj)
-        self.norm_feat_d,self.norm_feat_t=preprocess_features(feat)
+        self.norm_feat=preprocess_features(feat)
+        #数据属性
+        self.nb_nodes = self.norm_feat.shape[0]
+        self.ft_size = self.norm_feat.shape[1]
+        #转换成tensorfloat
+        self.norm_feat = torch.FloatTensor(self.norm_feat[np.newaxis])
+        self.norm_adj = torch.FloatTensor(self.norm_adj[np.newaxis])
+        self.truth= torch.FloatTensor(self.truth[np.newaxis])
+    
 
+    def docuda(self):
+        if torch.cuda.is_available():
+            split_line()
+            print('Using CUDA')
+            self.norm_feat = self.norm_feat.cuda()
+            self.norm_adj = self.norm_adj.cuda()
+            self.truth = self.truth.cuda()
+        else:
+            split_line()
+            print("Using CPU")
+    
+    def node_sub(self,subgraph_size):
+        added_adj_zero_row = torch.zeros((self.nb_nodes, 1, subgraph_size))
+        added_adj_zero_col = torch.zeros((self.nb_nodes, subgraph_size + 1, 1))
+        added_adj_zero_col[:,-1,:] = 1.
+        added_feat_zero_row = torch.zeros((self.nb_nodes, 1, self.ft_size))
+        if torch.cuda.is_available():
+            added_adj_zero_row = added_adj_zero_row.cuda()
+            added_adj_zero_col = added_adj_zero_col.cuda()
+            added_feat_zero_row = added_feat_zero_row.cuda()
+        split_line()
+        print("Subgraph has produced")
--- train.py
+++ train.py
@@ -7,5 +7,11 @@ from tqdm import tqdm
 import argparse
 
 from model import AE
-from data_process import RealDataset
+from data_process import *
 
+#载入数据
+data=RealDataset("cora")
+
+#模型
+data.docuda()
+data.node_sub(4)
